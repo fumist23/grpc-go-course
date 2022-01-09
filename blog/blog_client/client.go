8457a3f3f1b01a8bc5c14a299071663c5a28b92d
Fumiya Sato <fumist1028@gmail.com>
2022-01-09 23:02:52 +0900

    feat: create BlogClient

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -3,15 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
 
-	"github.com/simplesteph/grpc-go-course/blog/blogpb"
+	"github.com/fumist23/grpc-go-course/blog/blogpb"
 	"google.golang.org/grpc"
 )
 
 func main() {
-
 	fmt.Println("Blog Client")
 
 	opts := grpc.WithInsecure()
@@ -20,75 +18,23 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not connect: %v", err)
 	}
-	defer cc.Close() // Maybe this should be in a separate function and the error handled?
 
-	c := blogpb.NewBlogServiceClient(cc)
+	defer cc.Close()
 
-	// create Blog
-	fmt.Println("Creating the blog")
+	client := blogpb.NewBlogServiceClient(cc)
+
 	blog := &blogpb.Blog{
-		AuthorId: "Stephane",
-		Title:    "My First Blog",
-		Content:  "Content of the first blog",
+		AuthorId: "fumist23",
+		Title:    "satofumi story",
+		Content:  "forst content of blog",
 	}
-	createBlogRes, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	res, err := client.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{
+		Blog: blog,
+	})
+
 	if err != nil {
-		log.Fatalf("Unexpected error: %v", err)
-	}
-	fmt.Printf("Blog has been created: %v", createBlogRes)
-	blogID := createBlogRes.GetBlog().GetId()
-
-	// read Blog
-	fmt.Println("Reading the blog")
-
-	_, err2 := c.ReadBlog(context.Background(), &blogpb.ReadBlogRequest{BlogId: "5bdc29e661b75adcac496cf4"})
-	if err2 != nil {
-		fmt.Printf("Error happened while reading: %v \n", err2)
+		log.Fatalf("Failed to create blog.")
 	}
 
-	readBlogReq := &blogpb.ReadBlogRequest{BlogId: blogID}
-	readBlogRes, readBlogErr := c.ReadBlog(context.Background(), readBlogReq)
-	if readBlogErr != nil {
-		fmt.Printf("Error happened while reading: %v \n", readBlogErr)
-	}
-
-	fmt.Printf("Blog was read: %v \n", readBlogRes)
-
-	// update Blog
-	newBlog := &blogpb.Blog{
-		Id:       blogID,
-		AuthorId: "Changed Author",
-		Title:    "My First Blog (edited)",
-		Content:  "Content of the first blog, with some awesome additions!",
-	}
-	updateRes, updateErr := c.UpdateBlog(context.Background(), &blogpb.UpdateBlogRequest{Blog: newBlog})
-	if updateErr != nil {
-		fmt.Printf("Error happened while updating: %v \n", updateErr)
-	}
-	fmt.Printf("Blog was updated: %v\n", updateRes)
-
-	// delete Blog
-	deleteRes, deleteErr := c.DeleteBlog(context.Background(), &blogpb.DeleteBlogRequest{BlogId: blogID})
-
-	if deleteErr != nil {
-		fmt.Printf("Error happened while deleting: %v \n", deleteErr)
-	}
-	fmt.Printf("Blog was deleted: %v \n", deleteRes)
-
-	// list Blogs
-
-	stream, err := c.ListBlog(context.Background(), &blogpb.ListBlogRequest{})
-	if err != nil {
-		log.Fatalf("error while calling ListBlog RPC: %v", err)
-	}
-	for {
-		res, err := stream.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Something happened: %v", err)
-		}
-		fmt.Println(res.GetBlog())
-	}
+	fmt.Printf("Blog has been created! blog: %v", res.GetBlog())
 }
